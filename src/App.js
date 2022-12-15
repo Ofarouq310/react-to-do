e0dd01ae4fa84fdb4e7e3caeecf9019732e8aa72
Omar Farouq <ofarouq310@gmail.com>
2022-12-15 11:50:15 +0200

    Update main scirpt files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <h1>test</h1>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+/*eslint-disable*/
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+// component file
+import TodoContainer from './functionBased/components/TodoContainer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// stylesheet
+import './functionBased/App.css';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
